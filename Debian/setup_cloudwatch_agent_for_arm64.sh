67ae95d4e2997577ffa3b7e4b1f3495352f4c980
boming <boming0529@gmail.com>
2023-11-16 13:26:49 +0800

    [ADD] add setup cloudwetah agent for diff Architecture

---
+++ Debian/setup_cloudwatch_agent_for_arm64.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Update the system
+sudo apt-get update -y
+
+# Download and install Amazon CloudWatch Logs agent
+sudo wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/arm64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
+
+# Create a config.json file
+sudo bash -c 'cat > /opt/aws/amazon-cloudwatch-agent/bin/config.json' <<-'EOF'
+{
+	"metrics": {
+		"metrics_collected": {
+			"mem": {
+				"measurement": [
+					"mem_used_percent"
+				],
+				"metrics_collection_interval": 60
+			},
+			"disk": {
+				"measurement": [
+					"used_percent"
+				],
+				"metrics_collection_interval": 86400,
+				"resources": [
+					"*"
+				]
+			}
+		},
+		"append_dimensions": {
+			"InstanceId": "${aws:InstanceId}"
+		}
+	}
+}
+EOF
+
+# Start the CloudWatch agent
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
+
+sudo systemctl enable amazon-cloudwatch-agent
