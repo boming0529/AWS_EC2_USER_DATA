aacd7f4a2023570b570c118c8ecc121791628582
boming0529 <boming0529@gmail.com>
2023-11-16 13:14:55 +0800

    Create setup_postgres_client.sh

---
+++ Debian/setup_postgres_client.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt update
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/postgres.list'
+sudo apt update
+sudo apt install postgresql-client-15 -y
