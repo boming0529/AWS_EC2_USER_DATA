d4109e66d5344eb7e2bb2a8558f4e31e998a7308
boming <boming0529@gmail.com>
2024-01-05 16:22:12 +0800

    [FIX] fix setup podman

--- Debian/setup_podman.sh
+++ Debian/setup_podman.sh
@@ -1,9 +1,25 @@
-. /etc/os-release
-sudo sh -c "echo 'deb http://deb.debian.org/debian $VERSION_CODENAME-backports main' > /etc/apt/sources.list.d/backports.list"
-sudo apt-get update
-sudo apt-get -t $VERSION_CODENAME-backports install podman
-
+#!/bin/bash
 
 sudo apt update
+sudo apt install -y \
+    ca-certificates \
+    curl \
+    gpg \
+    gnupg \
+    lsb-release \
+    apt-transport-https \
+    software-properties-common
+
+
+source  /etc/os-release
+wget http://downloadcontent.opensuse.org/repositories/home:/alvistack/Debian_$VERSION_ID/Release.key -O alvistack_key
+cat alvistack_key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/alvistack.gpg  >/dev/null
+echo "deb http://downloadcontent.opensuse.org/repositories/home:/alvistack/Debian_$VERSION_ID/ /" | sudo tee  /etc/apt/sources.list.d/alvistack.list
+
+sudo apt update
+sudo apt-get -t $VERSION_CODENAME-backports install podman
+
 sudo apt install python3-pip -y
-pip install podman-compose
+# install podman and podman compose 
+sudo apt install podman python3-podman-compose  -y 
+sudo apt update
