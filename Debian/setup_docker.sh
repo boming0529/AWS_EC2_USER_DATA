adc29fb4ae47b72a60732a465fe06616ae13758e
boming <boming0529@gmail.com>
2025-05-22 15:09:37 +0800

    [UPD] upgrade docker compose version

--- Debian/setup_docker.sh
+++ Debian/setup_docker.sh
@@ -34,6 +34,6 @@ sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin -y
 usermod -aG docker admin
 
 # install Docker compose 
-sudo curl -L https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/v2.36.1/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
--- Ubuntu/setup_docker.sh
+++ Ubuntu/setup_docker.sh
@@ -24,5 +24,5 @@ sudo apt install docker-ce -y
 usermod -aG docker ubuntu
 
 # install Docker compose 
-sudo curl -L https://github.com/docker/compose/releases/download/v2.14.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/v2.36.1/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
