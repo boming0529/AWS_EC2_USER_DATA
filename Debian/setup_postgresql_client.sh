dcaf81909d8d7b6a8c2bae1afce3f9e1e07bfbc6
boming <boming0529@gmail.com>
2026-01-06 16:58:32 +0800

    [UPD] upgrade psql tool

--- Debian/setup_postgresql_client.sh
+++ Debian/setup_postgresql_client.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 sudo apt update
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/postgres.list'
+sudo apt install -y curl gnupg lsb-release
+
+curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | \
+  sudo gpg --dearmor -o /usr/share/keyrings/postgresql-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/postgresql-keyring.gpg] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" | \
+  sudo tee /etc/apt/sources.list.d/pgdg.list > /dev/null
+
 sudo apt update
-sudo apt install postgresql-client-15 -y
+sudo apt install -y postgresql-client-15
